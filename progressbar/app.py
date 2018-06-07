0ff877b185bcbb099decf8a8731175d02b92a365
Lena Schnedlitz <lena.schnedlitz@mail.de>
2018-06-07 22:01:00 +0200

    Refactor

--- progressbar/app.py
+++ progressbar/app.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template, send_file
-
 import io
 
-import format
+from flask import Flask, render_template, send_file
+from format import Format
 
 app = Flask(__name__)
 
@@ -16,14 +15,11 @@ def home():
 @app.route('/<int:dividend>/<int:divisor>')
 @app.route('/<int:dividend>/<int:divisor>/<palette>')
 def load(dividend, divisor=100, palette='TRAFFIC_LIGHT'):
-    percent = format.percentage(dividend, divisor)
-
-    with open('static/svg/progress.svg') as svg_file:
-        svg = svg_file.read()
-        buffer = io.BytesIO()
-        buffer.write(svg.format(percent).encode())
-        buffer.seek(0)
-        return send_file(buffer, mimetype='image/svg+xml')
+    svg = Format(dividend, divisor, palette).svg()
+    buffer = io.BytesIO()
+    buffer.write(svg.encode())
+    buffer.seek(0)
+    return send_file(buffer, mimetype='image/svg+xml')
 
 
 if __name__ == '__main__':
--- progressbar/format.py
+++ progressbar/format.py
@@ -1,7 +1,24 @@
-"""Helper functions"""
+""""""
 
 
-def percentage(dividend, divisor):
-    assert 0 <= dividend < divisor
-    assert 0 < divisor
-    return dividend / divisor * 100
+class Format:
+    def __init__(self, dividend, divisor, palette):
+        self.__validate(dividend, divisor, palette)
+        self.dividend = dividend
+        self.divisor = divisor
+        self.palette = palette
+
+    def percentage(self):
+        return self.dividend / self.divisor * 100
+
+    def svg(self):
+        with open('static/svg/progress.svg') as svg_file:
+            svg = svg_file.read()
+            return svg.format(self.percentage())
+
+    @staticmethod
+    def __validate(dividend, divisor, palette):
+        if not (0 <= dividend < divisor):
+            raise ValueError('Invalid dividend.')
+        if not (0 < divisor):
+            raise ValueError('Invalid divisor.')
