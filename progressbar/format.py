fc562f6c9ed89546f61c4765bdf5813b167ed257
Lena Schnedlitz <lena.schnedlitz@mail.de>
2018-06-10 23:22:55 +0200

    Clean up subclasses

--- progressbar/format.py
+++ progressbar/format.py
@@ -15,26 +15,31 @@ class Format:
         self.dividend = dividend
         self.divisor = divisor
         self.template = None
-        self._svg_parameters = {
-            'width': self.percentage(),
-            'text': self.text()
-        }
 
     def percentage(self):
         return self.dividend / self.divisor * 100
 
     def svg(self):
+        assert self.template, 'SVG template must be set.'
         file_path = os.path.join(SVG_PATH, self.template)
         with open(file_path) as svg_file:
             svg = svg_file.read()
-            return svg.format(**self._svg_parameters)
+            return svg.format(**self._svg_parameters())
 
     def text(self):
         text_format = '{}%' if self.divisor == 100 else '{}/{}'
         return text_format.format(self.dividend, self.divisor)
 
+    def _svg_parameters(self):
+        return {
+            'text': self.text(),
+            'width': self.percentage()
+        }
+
     @staticmethod
     def create(dividend, divisor, theme):
+        """Create and return a Format for the given theme"""
+
         Format.__validate(dividend, divisor)
         theme = theme.lower()
         try:
@@ -57,23 +62,23 @@ class Format:
             raise ValueError('Invalid divisor.')
 
 
-class DefaultFormat(Format):
+# FORMATS #####################################################################
+
+class GradientFormat(Format):
+    """A base format for progress bars with a gradient"""
+
     def __init__(self, dividend, divisor):
         super().__init__(dividend, divisor)
-        self.template = 'progress_default.svg'
-        self._svg_parameters = self.__svg_parameters()
+        self.template = 'progress_gradient.svg'
 
     def _colors(self):
-        return {
-            'start_color': '#165bc4',
-            'end_color': '#12499e'
-        }
+        raise NotImplementedError('')
 
-    def __svg_parameters(self):
+    def _svg_parameters(self):
         params = self._colors()
         params['text_alignment'] = self.__text_alignment()
 
-        return {**params, **self._svg_parameters}
+        return {**params, **super()._svg_parameters()}
 
     def __text_alignment(self):
         percentage = self.percentage()
@@ -84,11 +89,25 @@ class DefaultFormat(Format):
         return 'middle'
 
 
-class CustomColorFormat(DefaultFormat):
-    def __init__(self, dividend, divisor, color):
-        self.color = color
+class DefaultFormat(GradientFormat):
+    """The default format"""
 
+    def __init__(self, dividend, divisor):
         super().__init__(dividend, divisor)
+
+    def _colors(self):
+        return {
+            'start_color': '#165bc4',
+            'end_color': '#12499e'
+        }
+
+
+class CustomColorFormat(GradientFormat):
+    """A format that allows the usage of a custom base color"""
+
+    def __init__(self, dividend, divisor, color):
+        super().__init__(dividend, divisor)
+        self.color = color
 
     def _colors(self):
         return {
@@ -98,16 +117,24 @@ class CustomColorFormat(DefaultFormat):
 
 
 class MinimalFormat(Format):
+    """A format for minimal progress bars without any text or extras"""
+
     def __init__(self, dividend, divisor):
         super().__init__(dividend, divisor)
         self.template = 'progress_minimal.svg'
 
 
 class BadgeFormat(Format):
+    """A format for .svgs that look similar to the badges by shields.io"""
+
     def __init__(self, dividend, divisor):
         super().__init__(dividend, divisor)
         self.template = 'progress_badge.svg'
-        self._svg_parameters['color'] = self.__color()
+
+    def _svg_parameters(self):
+        params = super()._svg_parameters()
+        params['color'] = self.__color()
+        return params
 
     def __color(self):
         # Colors taken from shields.io
