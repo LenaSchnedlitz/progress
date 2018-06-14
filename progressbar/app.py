8466a31ece531ab55f0921178e7b4a1614d01d24
Lena Schnedlitz <lena.schnedlitz@mail.de>
2018-06-14 23:29:43 +0200

    Implement post request

--- progressbar/app.py
+++ progressbar/app.py
@@ -1,26 +1,42 @@
 import io
 
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, request, send_file
 
 from format import Format
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return render_template('main.html')
-
-
-@app.route('/<int:dividend>')
-@app.route('/<int:dividend>/<int:divisor>')
-@app.route('/<int:dividend>/<int:divisor>/<theme>')
-def load(dividend, divisor=100, theme='default'):
+def file(dividend, divisor, theme):
     svg = Format.create(dividend, divisor, theme).svg()
     buffer = io.BytesIO()
     buffer.write(svg.encode())
     buffer.seek(0)
     return send_file(buffer, mimetype='image/svg+xml')
+
+
+@app.route("/")
+def home():
+    return render_template('main.html')
+
+
+@app.route("/<int:dividend>")
+@app.route("/<int:dividend>/<int:divisor>")
+@app.route("/<int:dividend>/<int:divisor>/<theme>")
+def load(dividend, divisor=100, theme='default'):
+    return file(dividend, divisor, theme)
+
+
+@app.route("/", methods=['POST'])
+def redirect():
+    dividend = int(request.form['dividend'])
+    divisor = int(request.form['divisor'])
+
+    theme = request.form['theme']
+    if theme == "Custom Color":
+        theme = request.form['color'][1:]
+
+    return file(dividend, divisor, theme)
 
 
 if __name__ == '__main__':
