e486115d3c3fd4aca1b785c8d733837febb9c4a3
Lena Schnedlitz <lena.schnedlitz@mail.de>
2018-06-15 12:34:43 +0200

    Fix redirects and form validation

--- progressbar/app.py
+++ progressbar/app.py
@@ -1,16 +1,23 @@
 import io
 import logging
 
-from flask import Flask, abort, render_template, request, send_file
+from flask import Flask, abort, redirect, render_template, request, \
+    send_file, url_for
 
 from format import Format
 
 app = Flask(__name__)
 
 
-def file(dividend, divisor, theme):
-    """Return a progress bar .svg-file for the given parameters"""
+@app.route("/")
+def home():
+    return render_template('main.html')
 
+
+@app.route("/<int:dividend>")
+@app.route("/<int:dividend>/<int:divisor>")
+@app.route("/<int:dividend>/<int:divisor>/<theme>")
+def file(dividend, divisor=100, theme='default'):
     try:
         svg = Format.create(dividend, divisor, theme).svg()
         buffer = io.BytesIO()
@@ -22,33 +29,25 @@ def file(dividend, divisor, theme):
         abort(404)
 
 
+@app.route("/", methods=['POST'])
+def redirect_to_file():
+    params = request.form.to_dict()
+
+    params['theme'] = params['theme'].lower() \
+        if params['theme'] != "Custom Color" \
+        else params['color'][1:]
+
+    return redirect("/{dividend}/{divisor}/{theme}".format(**params))
+
+
 @app.errorhandler(404)
 def file_not_found(_):
-    return render_template('error.html'), 404
+    return redirect(url_for('error'))
 
 
-@app.route("/")
-def home():
-    return render_template('main.html')
-
-
-@app.route("/<int:dividend>")
-@app.route("/<int:dividend>/<int:divisor>")
-@app.route("/<int:dividend>/<int:divisor>/<theme>")
-def load(dividend, divisor=100, theme='default'):
-    return file(dividend, divisor, theme)
-
-
-@app.route("/", methods=['POST'])
-def redirect():
-    dividend = int(request.form['dividend'])
-    divisor = int(request.form['divisor'])
-
-    theme = request.form['theme']
-    if theme == "Custom Color":
-        theme = request.form['color'][1:]
-
-    return file(dividend, divisor, theme)
+@app.route("/error")
+def error():
+    return render_template('error.html')
 
 
 if __name__ == '__main__':
